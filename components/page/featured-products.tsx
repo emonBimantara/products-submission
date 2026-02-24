88364b7080d8a00584bc87ee5e0c85c547321033
emonBimantara <shelmondbimantara@gmail.com>
2026-02-24 20:59:07 +0700

    first commit

---
+++ components/page/featured-products.tsx
@@ -0,0 +1,11 @@
+export default function FeaturedProducts() {
+    return (
+        <section className="py-20 bg-muted/20">
+            <div className="wrapper">
+                <div className="flex items-center justify-between mb-8">
+                    <h1>Featured Products</h1>
+                </div>
+            </div>
+        </section>
+    )
+}
